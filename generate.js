3052aa9c2b32d6697afb4da97b53f81d833f1150
udaycoding <udaydolare9@gmail.com>
2025-09-01 17:34:44 +0530

    fix: removed duplicate generate.js

--- generate.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json({ message: "API working fine 🚀" });
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
-  }
-}
